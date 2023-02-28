8032174e46be1ff35456c7eb1858a163f69ae12c
CodeLordGh <babohkossi228@icloud.com>
2023-02-28 18:46:19 +0000

    FrontEnd home, login, signup page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import {Row, Col, Button} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import "./bg.css";
 
 function homePage() {
   return (
-    <div>homePage</div>
+    <Row>
+      <Col md={6} className= 'd-flex flex-direction-column align-items-center justify-content-center'>
+        <div>
+        <h1>Share your coding experience with friends</h1>
+          <p>LetsCode lets you connect with your friends</p>
+          <LinkContainer to='/chat'>
+            <Button variant='success'>Get Started
+              <i className='fas fa-comments home-message-icon'></i>
+            </Button>
+          </LinkContainer>
+        </div>
+      </Col>
+      <Col md={6} className='Home__bg'>
+
+      </Col>
+    </Row>
   )
 }
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Container, Row, Col, Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import './signup.css';
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <Container>
+      <Row>
+        <Col d-flex align-items-center justify-content-center flex-direction-column>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Your name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Signup
+          </Button>
+    </Form>
+        </Col>
+        <Col md={5} className='signup__bg'></Col>
+      </Row>
+    </Container>
   )
 }
 
